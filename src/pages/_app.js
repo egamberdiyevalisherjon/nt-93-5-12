95d9ba3d863871ec50e4600f9328ebd6aeb58d66
Alisherjon Egamberdiyev <egamberdiyevalisherjon@gmail.com>
2023-03-06 16:15:18 +0500

    getserversideprop

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,5 @@ import "@/styles/globals.css";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>My first Next app</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
--- src/pages/products/[id].js
+++ src/pages/products/[id].js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
-const DetailedInfo = () => {
-  const router = useRouter();
-  console.log(router.query.id);
+const DetailedInfo = ({ product }) => {
+  useEffect(() => console.log(product), [product]);
+  // const router = useRouter();
+  // console.log(router.query.id);
 
-  return <div>DetailedInfo</div>;
+  return (
+    <div>
+      <Head>
+        <title>profile for {product.name}</title>
+        <meta name="website" content={product.website} />
+      </Head>
+      DetailedInfo
+    </div>
+  );
 };
 
 export default DetailedInfo;
+
+export async function getServerSideProps(context) {
+  let id = context.query.id;
+
+  let data = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  ).then((res) => res.json());
+  return {
+    props: {
+      product: data,
+    },
+  };
+}
