e4404bfe47cb1050ff93b015e35173318fcbb7d9
Alisherjon Egamberdiyev <egamberdiyevalisherjon@gmail.com>
2023-03-06 16:29:27 +0500

    get static props

--- src/pages/products/[id].js
+++ src/pages/products/[id].js
@@ -3,15 +3,15 @@ import { useRouter } from "next/router";
 import Head from "next/head";
 
 const DetailedInfo = ({ product }) => {
-  useEffect(() => console.log(product), [product]);
+  // useEffect(() => console.log(product), [product]);
   // const router = useRouter();
   // console.log(router.query.id);
 
   return (
     <div>
       <Head>
-        <title>profile for {product.name}</title>
-        <meta name="website" content={product.website} />
+        {/* <title>profile for {product.name}</title>
+        <meta name="website" content={product.website} /> */}
       </Head>
       DetailedInfo
     </div>
@@ -20,15 +20,32 @@ const DetailedInfo = ({ product }) => {
 
 export default DetailedInfo;
 
-export async function getServerSideProps(context) {
-  let id = context.query.id;
+// export async function getServerSideProps(context) {
+//   let id = context.query.id;
 
-  let data = await fetch(
-    `https://jsonplaceholder.typicode.com/users/${id}`
-  ).then((res) => res.json());
+//   let data = await fetch(
+//     `https://jsonplaceholder.typicode.com/users/${id}`
+//   ).then((res) => res.json());
+//   return {
+//     props: {
+//       product: data,
+//     },
+//   };
+// }
+
+// Prerendering
+export async function getStaticProps(context) {
+  console.log(context);
+  return { props: {} };
+}
+
+export async function getStaticPaths() {
+  let data = await fetch("https://jsonplaceholder.typicode.com/users").then(
+    (res) => res.json()
+  );
+
   return {
-    props: {
-      product: data,
-    },
+    paths: data.map((user) => ({ params: { id: user.id + "" } })),
+    fallback: true,
   };
 }
